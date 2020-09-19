4c3dcd585b0da3938fe6fa5253db2c81a7cb7388
snlucas <lucas.lambda.101@gmail.com>
2020-09-19 16:37:39 -0300

    Adiciona implementacao para mostrar mapa da sala

--- src/reserva/ControladorReserva.java
+++ src/reserva/ControladorReserva.java
@@ -31,11 +31,17 @@ public class ControladorReserva implements InterfaceControladorReserva {
         return this.listReservas;
     }
 
-    public ArrayList<String> getCodReservas(ArrayList<Reserva> listReservas) {
+    /**
+     * 
+     * Metodo getCodReservas
+     * Extrai codigos de assentos da lista de reservas
+     * @return lista com codigos de assentos
+     */
+    public ArrayList<String> getCodReservas() {
         ArrayList<String> codReservas = new ArrayList<>();
 
-        for (int i = 0; i < listReservas.size(); i++)
-            codReservas.add(listReservas.get(i).getCodigoAssento());
+        for (int i = 0; i < this.getListReservas().size(); i++)
+            codReservas.add(this.getListReservas().get(i).getCodigoAssento());
 
         return codReservas;
     }
@@ -57,17 +63,96 @@ public class ControladorReserva implements InterfaceControladorReserva {
         return this.listReservas.remove(reserva);
     }
 
+    
     @Override
-    public void mostrarMapaDaSala(ArrayList<Reserva> listReservas) {
-        String[] fileiras = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
+    public void mostrarMapaDaSala() {
+        String[] fileiras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
         String linha = " ______________________________________________________      ______________________________________________________ ";
         String[][] mapa = new String[fileiras.length + 1][17];
-        ArrayList<String> codAssentos = this.getCodReservas(listReservas);
-
+        String[] codAssentos = new String[this.getListReservas().size()];
+        
+        for (int i = 0; i < this.getListReservas().size(); i++)
+            codAssentos[i] = this.getListReservas().get(i).getCodigoAssento(); 
+        
 
         System.out.println(linha);
         for (int i = 0; i <= fileiras.length; i++) {
             for (int j = 0; j < mapa[0].length; j++) {
+                // Desenha numero das fileiras
+                if (i == fileiras.length) {
+                    int x = 0;  // ajuda a controlar index de numeros
+                    if (j > 0 && j < 16) {
+                        if (j == 8) {
+                            // Coloca espacamento entre colunas (conjunto de 7)
+                            mapa[i][j] = "    ";
+                            x = 2;
+                        } else {
+                            // Coloca numero de identificacao
+                            if (j < 10) {
+                                //mapa[i][j] = String.format("|__%d__|", (j - x));
+                                mapa[i][j] = "|__" + x + "__|";
+                            } else {
+                                //mapa[i][j] = String.format("|__%d_|", (j - x));
+                                mapa[i][j] = "|__" + x + "_|";
+                            }
+                        }
+                    } else {
+                        // Coloca espacamento inicial e final
+                        mapa[i][j] = "       ";
+                    }
+                } else {
+                    for (String cod : codAssentos) {
+                        if (j == 0 || j == 16) {
+                            // Coloca letra de identificacao da fileira
+                            mapa[i][j] = String.format("|__%s__|", fileiras[i]);
+                        } else if (j == 8) {
+                            // Coloca espacamento entre colunas (conjunto de 7)
+                            mapa[i][j] = "    ";
+                        } else {
+                            if ((cod.split("")[0].equals(fileiras[i])) && (cod.split("")[1].equals(Integer.toString(j)))) {
+                                // Assento ocupado
+                                mapa[i][j] = "|__X__|";
+                                break;
+                            } else {
+                                // Assento livre
+                                mapa[i][j] = "|__ __|";
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        for (String[] l : mapa) {
+            for (String c : l) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 
+     * Metodo getMapaDaSala
+     * Atribui a uma matriz 
+     * todo o mapa da Sala com
+     * assentos livres/ocupados
+     * 
+     * @return mapa da sala
+     */
+    /*
+    public String[][] getMapaDaSala() {
+        String[] fileiras = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
+        String linha = " ______________________________________________________      ______________________________________________________ ";
+        String[][] mapa = new String[fileiras.length + 2][18];
+        ArrayList<String> codAssentos = this.getCodReservas();
+
+
+        mapa[0][0] = linha;
+        for (int i = 1; i <= fileiras.length; i++) {
+            for (int j = 1; j < mapa[0].length; j++) {
                 // Desenha numero das fileiras
                 if (i == fileiras.length) {
                     if (j > 0 && j < 16) {
@@ -109,5 +194,8 @@ public class ControladorReserva implements InterfaceControladorReserva {
             }
         }
 
+        return mapa;
+
     }
+    */
 }
